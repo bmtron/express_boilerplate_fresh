e0dd960b2666c562cf60730eccab18ee4aa24f3f
Brendan M Meehan <bmmeehan3@gmail.com>
2020-12-20 22:51:30 -0500

    passkeep_api stuff

--- src/app.js
+++ src/app.js
@@ -4,17 +4,42 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const sql = require('mssql')
 
 const app = express()
 
 const morganOption = (NODE_ENV === 'production' ? 'tiny' : 'common')
 
+var sqlconfig = {
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    server: process.env.SERVER,
+    database: process.env.DATABASE
+}
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
 
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!')
+})
+app.get('/logins', function (req, res) {
+    sql.connect(sqlconfig, function() {
+        var request = new sql.Request();
+        request.query('select * from logins', function(err, recordset) {
+            if (err) console.log(err)
+            res.end(JSON.stringify(recordset))
+        })
+    })
+})
+app.get('/masterlogins', function(req, res) {
+    sql.connect(sqlconfig, function() {
+        var request = new sql.Request();
+        request.query('select * FROM master_login', function(err, recordset) {
+            if (err) console.log(err)
+            res.end(JSON.stringify(recordset))
+        })
+    })
 })
 app.use(function errorHandler(error, req, res, next) {
     let response
